2ff290cae9acf0662b8bdd7a0465ced1d5f2ec43
Arthur Bernardo <arthurutschgg@gmail.com>
2021-11-13 18:40:36 -0300

    feat: Getting values from the login form

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,20 +1,59 @@
 import Header from "../../components/Header/Header";
 import styles from "./SignIn.module.scss";
-const SignIn = () => {
+import React, {ChangeEvent, useState} from "react";
+import api from "../../api/api";
+
+type SignInProps = {
+  email: string
+  password: string
+}
+
+const SignIn: React.FC = () => {
+
+  const [state, setState] = useState<SignInProps>({
+    email: '', password: ''
+  })
+
+  const updateLoginData = (e: ChangeEvent<HTMLInputElement>) => {
+      setState({
+        ...state,
+        [e.target.name]: e.target.value,
+      })
+  }
+
+  const HandleOnSubmit = async (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    console.log(state)
+    const response = await api.post('login', {
+      headers: {
+
+      },
+    body: {state},
+    })
+    console.log(response)
+  }
+
+
+
+
+
   return (
     <>
       <Header />
       <div className={styles.container}>
-        <form className={styles.formContainer}>
+        <form className={styles.formContainer} onSubmit={HandleOnSubmit}>
           <h1>Faça Login</h1>
           <input
             type={"email"}
             name={"email"}
+            onChange={(e:ChangeEvent<HTMLInputElement>) => updateLoginData(e)}
             placeholder={"Digite seu email"}
           />
           <input
             type={"password"}
             name={"password"}
+            onChange={(e:ChangeEvent<HTMLInputElement>) => updateLoginData(e)}
             placeholder={"Digite sua senha"}
           />
           <button className={styles.button} type="submit">
